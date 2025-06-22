fa3bb0c4a48914489110bbd143455788f219439a
Maria Morozova <59123858+mariamorozovaa@users.noreply.github.com>
2025-06-22 15:56:28 +0300

    Complete general task

--- script.js
+++ script.js
@@ -1,36 +1,35 @@
-const container = document.querySelector("#container");
-
 const btns = document.querySelector("#buttons");
 const btnSetSize = document.createElement("button");
 btnSetSize.textContent = "Set grid size";
+btnSetSize.setAttribute("style", `width: 90px; height: 50px; background-color: pink `);
+
 btns.appendChild(btnSetSize);
 let setSize = 16;
 createGrid(setSize);
 
 btnSetSize.addEventListener("click", () => {
-  setSize = prompt("Enter the size of grid (<100)");
+  setSize = prompt("Enter the size of grid (<100)", 16);
 
   if (setSize < 100) {
     createGrid(setSize);
   } else {
     alert("The number is more than 100 or equal. Please enter a correct number");
   }
-  // for (let i = 0; i < setSize; i++) {
-  //   for (let j = 0; j < setSize; j++) {
-  //     container.removeChild(content);
-  //   }
-  // }
 });
 
 function createGrid(setSize) {
-  for (let i = 0; i < setSize; i++) {
-    for (let j = 0; j < setSize; j++) {
-      const content = document.createElement("div");
-      content.classList.add("square");
-      content.setAttribute("style", `width: ${600 / setSize}px; height: ${600 / setSize}px;`);
+  const container = document.querySelector("#container");
 
-      container.appendChild(content);
-      content.addEventListener("mouseover", () => content.classList.add("hoverSquare")); //mousemove
-    }
+  while (container.firstChild) {
+    container.removeChild(container.firstChild);
+  }
+
+  for (let j = 0; j < setSize ** 2; j++) {
+    const content = document.createElement("div");
+    content.classList.add("square");
+    content.setAttribute("style", `width: ${600 / setSize}px; height: ${600 / setSize}px;`);
+
+    container.appendChild(content);
+    content.addEventListener("mouseover", () => content.classList.add("hoverSquare")); //mousemove
   }
 }
